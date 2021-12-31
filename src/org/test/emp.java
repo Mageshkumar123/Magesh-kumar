1761d413d22e3092f64e250aad7a1a3632822011
Mageshkumar123 <mahesh00666@gmail.com>
2021-12-31 16:49:11 +0530

    message

---
+++ src/org/test/emp.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class emp {
+	private void add() {
+	 int a=2;
+	 System.out.println(a);
+		
+
+	}
+	public static void main(String[] args) {
+		emp e=new emp();
+		e.add();
+	}
+
+}
